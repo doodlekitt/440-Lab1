7b0794e188191666598fa3453eca4c8e8b3c85ea
Rhiannon Malia <rmalia@andrew.cmu.edu>
2013-09-07 15:34:00 -0400

    Added code

--- GlobalManager.java
+++ GlobalManager.java
@@ -1,3 +1,15 @@
+import java.io.DataInputStream;
+import java.io.PrintStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.util.Hashtable;
+
 public class GlobalManager {
+
+    public static void main(String args[]) {
+	private static ServerSocket GM = null;
+	Hashtable<String, Socket> clients = new Hashtable<String, Socket>();
+    }
 
 }
