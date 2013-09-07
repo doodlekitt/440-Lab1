5e2ef914a344e6d417a387318ef4fe14609ad113
Rhiannon Malia <rmalia@andrew.cmu.edu>
2013-09-07 18:03:20 -0400

    More code and comments

--- ProcessManager.java
+++ ProcessManager.java
@@ -8,21 +8,47 @@ public class ProcessManager {
 	System.out.println("Create New Process: new <Class> <nickname>\n Move Process: migrate <nickname> <target>\n Server Client Query: query\n Exit Program: quit\n"); 
    	
 	BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+	private DataInputStream is = null;
+	private PrintStream os = null;
+	private Socket PMSocket = null;
+	
 
 	String command = null;
 
 	\\ Needs to read in commands and execute until it is told to die
+	\\ THE EXECUTE COMMAND SHOULD MAKE AND START THE THING
+	\\ WHERE DO WE STORE THEM THOUGH AND IF WE STORE WE SHOULD
+	\\ REMOVE WHEN THEY ARE DONE
 
-	while(command != "quit"){
+	\\ Basically, the PM has to check both the buffered reader for input 
+	\\ the cmd line and check the socket input stream for messages 
+	\\ to act on from the server
+
+	try {
 	    command = br.readLine();
-	    execute(command);
+	    while(command != null){
+		if(command.startsWith("quit"))
+		{
+		    break;
+		}
+		else{
+		    execute(command);
+		}
+	    }
 	} catch (IOException e) {
 	    System.out.println(e);
 	}
+	
+	// Clean up is last
+	os.close();
+	is.close();
+	br.close();
+	PMSocket.close();
+
     }
 
     public void execute(String command) {
-
+	if(command.startsWith("new"));
     }
 
 }
