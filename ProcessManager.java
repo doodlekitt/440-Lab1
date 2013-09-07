e331510dce3c5bf9b92dd92c891bc37d5643b462
Rhiannon Malia <rmalia@andrew.cmu.edu>
2013-09-07 16:56:54 -0400

    More codey things `

--- ProcessManager.java
+++ ProcessManager.java
@@ -1,3 +1,28 @@
+import java.io.*;
+
 public class ProcessManager {
+
+    public static void main (String[] args) {
+	System.out.println("Available Commands");
+
+	System.out.println("Create New Process: new <Class> <nickname>\n Move Process: migrate <nickname> <target>\n Server Client Query: query\n Exit Program: quit\n"); 
+   	
+	BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+
+	String command = null;
+
+	\\ Needs to read in commands and execute until it is told to die
+
+	while(command != "quit"){
+	    command = br.readLine();
+	    execute(command);
+	} catch (IOException e) {
+	    System.out.println(e);
+	}
+    }
+
+    public void execute(String command) {
+
+    }
 
 }
