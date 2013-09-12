e7fbf8fc9b7320fc45062dfe9bfc791db3d25024
Rhiannon Malia <rmalia@andrew.cmu.edu>
2013-09-11 20:20:52 -0400

    Did Transactional thing

---
+++ TransactionalFileInputStream.java
@@ -0,0 +1,44 @@
+import java.io.*;
+
+public class TransactionFileInputStream extends java.io.InputStream implements java.io.Serializable {
+    private long pos = 0;
+    private String file_name;
+    
+    public TransactionFileInputStream(String filename){
+	super(); // needed?
+	file_name = filename;
+    }
+
+    public synchronized int read() throws IOException {
+	int next;
+	RandomAccessFile reader = new RandomAccessFile(this.file_name, "r");
+	reader.seek(pos);
+	next = reader.read();
+	reader.close();
+	this.pos++;
+	return next;
+	
+    }
+
+    public synchronized int read(byte[] b) throws IOException{
+	int ans;
+	RandomAccessFile reader = new RandomAccessFile(this.file_name, "r");
+	reader.seek(pos);
+	ans = reader.read(b);
+	reader.close();
+	this.pos += ans;
+	return ans;
+    }
+
+    public synchronized int read(byte[] b, int off, int len) throws IOException{
+	int ans; 
+	RandomAccessFile reader = new RandomAccessFile(this.file_name, "r");
+	reader.seek(pos);
+	ans = reader.read(b, off, len);
+	reader.close();
+	this.pos += ans;
+	return ans;
+    }
+
+
+}
---
+++ TransactionalFileOutputStream.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.lang.*;
+
+public class TransactionFileOutputStream extends java.io.OutputStream implements java.io.Serializable {
+
+    private long pos = 0;
+    private String file_name;
+
+    public TransactionFileOutputStream (String filename){
+	super(); // needed?
+	file_name = filename;
+    }
+
+    public synchronized void write (int b) throws IOException {
+	RandomAccessFile writer = new RandomAccessFile (this.file_name, "w"); 
+	writer.seek(this.pos);
+	writer.write(b);
+	writer.close();
+	this.pos++;
+    }
+
+    public synchronized void write(byte[] b) throws IOException 
+    {
+	RandomAccessFile writer = new RandomAccessFile(this.file_name, "w");
+	writer.seek(this.pos);
+	writer.write(b);
+	writer.close();
+	this.pos += b.length;
+    }
+
+    public synchronized void write (byte[] bytes, int off, int len) throws IOException {
+    	RandomAccessFile writer = new RandomAccessFile (this.file_name, "w");
+	writer.seek(this.pos);
+	writer.write(bytes, off, len);
+	writer.close();
+	this.pos += len;	 
+
+    }
+
+}
