306e7918626fc167e9a37b05ee459297c0ec457b
Shannon Williams <srw@andrew.cmu.edu>
2013-09-12 12:01:51 -0400

    Cleaning up slave

--- Slave.java
+++ Slave.java
@@ -1,48 +1,58 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.PrintStream;
-import java.io.IOException;
-import java.net.Socket;
+import java.lang.ClassNotFoundException;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Hashtable;
 
 public class Slave implements java.io.Serializable {
 
-    private static int port = 0;
     private static String pmHost = "";
     private static int pmPort = 0;
 
     private static Socket PM = null;
-
+    private static ObjectInputStream is = null;
     private static ObjectOutputStream os = null;
 
     public static void main(String args[]) throws IOException {
 
         // Parse args
-        int portNumber = -1;
-        String masterHost = null;
-	
-	if(args.length < 3) {
+	if(args.length < 2) {
 	    System.out.println("Expecting arguements of the form:");
-            System.out.println("slave <slave port> <host> <PM port>");
-            System.out.println("Where <slave port> is the port for the new Slave and <host> and <master port> show the location of the Process Manager");
+            System.out.println("slave <pmHost> <pmPort>");
             return;
 	}
 	else {
-	    port = Integer.valueOf(args[0]).intValue();
-            pmHost = args[1];
-            pmPort = Integer.valueOf(args[2]).intValue();
+            pmHost = args[0];
+            pmPort = Integer.valueOf(args[1]).intValue();
 	}
 
         // Set up sockets for listening and sending
 	try {
             PM = new Socket(pmHost, pmPort);
+            is = new ObjectInputStream(PM.getInputStream());
             os = new ObjectOutputStream(PM.getOutputStream());
 	} catch (IOException e) {
 	    System.out.println(e);
 	}
 
+        SlavePackage send = new SlavePackage();
+        PMPackage recieve = null;
+
+        os.writeObject(send);
+
+        try {
+            recieve = (PMPackage)is.readObject();
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println(e);
+        }
+
         try {
             PM.close();
+            is.close();
             os.close();
         } catch (IOException e) {
             System.out.println(e);
