bf35ea3f3ce9cb7f99a8a5d9ea46b95d8f197481
Rhiannon Malia <rmalia@andrew.cmu.edu>
2013-09-12 18:08:27 -0400

    Some ideas and things

--- PMPackage.java
+++ PMPackage.java
@@ -1,3 +1,24 @@
 public class PMPackage {
 
+    private String command; // migrate or start
+    private String target; // where to send it, can be empty
+    private String process; //
+
+    public PMPackage(String comm, String tar, String proc){
+	command = comm;
+	target = tar;
+	process = proc;
+    }
+
+    public String command(){
+	return this.command;
+    }
+
+    public String target(){
+	return this.target;
+    }
+
+    public String process(){
+	return this.process;
+    }
 }
--- SlavePackage.java
+++ SlavePackage.java
@@ -1,3 +1,21 @@
-public class SlavePackage {
 
+public class SlavePackage {
+    
+    private String message;
+    private String target;
+
+    // should have file path as message
+    // some way to indicate the target for the process migration
+    public SlavePackage(String tar, String mes){
+	message = mes;
+	target = tar;
+    }
+
+    public String message(){
+	return this.message;
+    }
+
+    public String target(){
+	return this.target;
+    }
 }
